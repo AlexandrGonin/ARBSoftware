98d8d0940807e3190f7998f5ae2f6db66eafbdc4
Stepwz <150243762+Stepwz@users.noreply.github.com>
2025-03-15 21:10:18 +0500

    Update TGbot.py

--- TGbot.py
+++ TGbot.py
@@ -3,14 +3,16 @@ sys.path.append("dex screener")
 sys.path.append("gmgn")
 sys.path.append("mexc")
 import dex_screener_api as ds
-import gmgn_api as gmgn
+#import gmgn_api as gmgn
 import mexc_api as mexc
-
 
 import asyncio
 import logging
+import aiogram
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
+from aiogram.utils.markdown import bold
+import aiogram.utils.markdown as fmt
 
 logging.basicConfig(level=logging.INFO)
 # Объект бота
@@ -23,24 +25,43 @@ dp = Dispatcher()
 #print("Dex Screener price: ", ds.GetDexScreenerPrice("solana", "JUPyiwrYJFskUPiHa7hkeR8VUtAeFoSYbKedZNsDvCN"))
 #print("GMGN AI price: ", gmgn.GetGmgnPrice("https://gmgn.ai/sol/token/JUPyiwrYJFskUPiHa7hkeR8VUtAeFoSYbKedZNsDvCN"))
 
-
+# types.InlineKeyboardButton(text="GMGN", url="https://gmgn.ai/sol/token/JUPyiwrYJFskUPiHa7hkeR8VUtAeFoSYbKedZNsDvCN"),
+#        types.InlineKeyboardButton(text="MEXC", url="https://futures.mexc.com/ru-RU/exchange/JUP_USDT?utm_source=mexc&utm_medium=pagehowtobuyfuturesbuttonJUP&utm_campaign=pagefuturesJUP&inviteCode=mexc-HtbFuBuFutu")
+#    ]
+#
 @dp.message(Command("start"))
 async def cmd_start(message: types.Message):
     mexc_fut = mexc.get_futures_mexc_price("JUP_USDT") 
     mexc_spot = mexc.get_spot_mexc_price("JUPUSDT")
     dex_price = ds.GetDexScreenerPrice("solana", "JUPyiwrYJFskUPiHa7hkeR8VUtAeFoSYbKedZNsDvCN")
     spred1 = round(100 - ((float(mexc_fut)/float(dex_price))*100), 5)
-    await message.answer("Price:")
-    await message.answer("Mexc futures: "+str(mexc_fut))
-    await message.answer("Mexc spot: "+str(mexc_spot))
-    await message.answer("Dex price: "+str(dex_price))
-    if spred1>0:
-        await message.answer("Спред (fut-dex): "+str(spred1))
-    elif spred1==0:
-        await message.answer("Спред = 0")
-    else:
-        await message.answer("Спред < 0")
-#    await message.answer(str(gmgn.GetGmgnPrice()))
+    spred2 = round(100 - ((float(mexc_spot)/float(dex_price))*100), 5)
+    
+    await message.answer(
+        fmt.text(
+            fmt.text(fmt.hbold("Token:  JUP")),
+            fmt.text(fmt.hbold("MEXC Spot"), (mexc_spot)),
+            fmt.text(fmt.hbold("MEXC-DEX"),  spred1),
+            fmt.text(fmt.hbold("SPOT-DEX"), spred2),
+            fmt.text(fmt.hbold("MEXC Futures"), mexc_fut),
+            fmt.text(fmt.hbold("DEX Screener"), dex_price),
+            sep="\n\n", 
+
+        ), parse_mode="HTML"
+    )
+
+@dp.message(Command("start"))
+async def cmd_inline_url(message: types.Message):
+    """Отправляет сообщение с inline кнопками-ссылками."""
+    buttons = [
+        types.InlineKeyboardButton(text="GitHub", url="https://github.com"),
+        types.InlineKeyboardButton(text="Оф. канал Telegram", url="tg://resolve?domain=telegram")
+    ]
+
+    # Создаем inline_keyboard как список списков.
+    keyboard = types.InlineKeyboardMarkup(inline_keyboard=[buttons])  # Все кнопки в одной строке
+
+    await message.answer("Кнопки-ссылки", reply_markup=keyboard)
 
 
 # Запуск процесса поллинга новых апдейтов
